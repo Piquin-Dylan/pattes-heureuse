92d5a1f6a84f646d218049a419243b1aa09d7855
Piquin-Dylan <dylan.piquin@student.hepl.be>
2025-12-24 17:06:26 +0100

    add required input

--- app/Livewire/Volunteers.php
+++ app/Livewire/Volunteers.php
@@ -2,12 +2,32 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Volunteers extends Component
 {
+
+    #[Validate('required')]
+    public $name = "";
+
+    #[Validate('required')]
+    public $email = "";
+
+    #[Validate('required')]
+    public $tel = "";
+
+    #[Validate('required')]
+    public $role = "";
+
     public function render()
     {
         return view('livewire.volunteers');
+    }
+
+    public function submit()
+    {
+
+        $this->validate();
     }
 }
--- resources/views/livewire/volunteers.blade.php
+++ resources/views/livewire/volunteers.blade.php
@@ -1,19 +1,24 @@
 <div>
     <h1>Salut les volunteers</h1>
 
-    <form class="flex flex-col gap-5">
+    <form wire:submit.prevent="submit" class="flex flex-col gap-5">
+
         <label for="name">Nom du bénévole</label>
-        <input type="text" id="name" placeholder="Entrez le nom d'utilisateur">
+        <input wire:model="name" type="text" id="name" placeholder="Entrez le nom d'utilisateur">
+        @error('name')<span class="error">{{$message}}</span> @enderror
 
         <label class="" for="name">Adresse mail</label>
-        <input type="text" id="name" placeholder="Entrez l'adresse mail">
+        <input wire:model="email" type="text" id="name" placeholder="Entrez l'adresse mail">
+        @error('email') <span class="error">{{$message}}</span> @enderror
 
         <label for="tel">Numéros de téléphone</label>
         <input type="tel" id="tel" placeholder="Entrez le mot de passe">
+        @error('tel') <span class="error">{{$message}}</span> @enderror
 
         <label>Role</label>
         <select>
             <option></option>
+            @error('role') <span class="error">{{$message}}</span> @enderror
         </select>
 
         <label>Image de profil</label>
