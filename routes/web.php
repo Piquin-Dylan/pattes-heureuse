b245f25e8a05490eef78eee97cddc96f17b7b949
Piquin-Dylan <dylan.piquin@student.hepl.be>
2026-01-02 00:30:42 +0100

    correction route v2

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,16 @@ use App\Livewire\DescriptionAnimal;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::domain('pattes-heureuse.test')->group(function () {
-    Route::view('/home', 'client.home')->name('client.home');
-    Route::view('/about', 'client.about')->name('client.about');
-    Route::view('/volunteers', 'client.volunteers')->name('client.volunteers');
-    Route::view('/adoption', 'client.adoption')->name('client.adoption');
-    Route::view('/descriptionAnimal', 'client.descriptionAnimal')->name('client.descriptionAnimal');
-    Route::view('/contact', 'client.contact')->name('client.contact');
-});
+// Routes client
+Route::view('/home', 'client.home')->name('client.home');
+Route::view('/about', 'client.about')->name('client.about');
+Route::view('/volunteers', 'client.volunteers')->name('client.volunteers');
+Route::view('/adoption', 'client.adoption')->name('client.adoption');
+Route::view('/descriptionAnimal', 'client.descriptionAnimal')->name('client.descriptionAnimal');
+Route::view('/contact', 'client.contact')->name('client.contact');
 
-Route::domain('admin.pattes-heureuse.test')->middleware(['auth'])->group(function () {
+// Routes admin
+Route::middleware(['auth'])->group(function () {
     Route::view('/dashboard', 'dashboard')->name('dashboard');
 
     Route::get('/greeter', Greeter::class)->name('greeter');
